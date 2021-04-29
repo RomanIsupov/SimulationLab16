5536e48452a8ae4992ddc94de7967b1cb8623621
RomanIsupov <romisup@gmail.com>
2021-04-30 00:23:29 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace SimulationLab16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.start = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.inputPrice = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Buy = new System.Windows.Forms.Button();
+            this.Sell = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Roubles = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Currency = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputPrice)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.start.Location = new System.Drawing.Point(154, 31);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(106, 45);
+            this.start.TabIndex = 0;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(16, 106);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.Green;
+            series1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            series1.IsValueShownAsLabel = true;
+            series1.LabelFormat = "f4";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(952, 366);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // inputPrice
+            // 
+            this.inputPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputPrice.Location = new System.Drawing.Point(16, 50);
+            this.inputPrice.Name = "inputPrice";
+            this.inputPrice.Size = new System.Drawing.Size(120, 26);
+            this.inputPrice.TabIndex = 2;
+            this.inputPrice.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(-1, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Currency start value";
+            // 
+            // Buy
+            // 
+            this.Buy.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Buy.Location = new System.Drawing.Point(318, 56);
+            this.Buy.Name = "Buy";
+            this.Buy.Size = new System.Drawing.Size(105, 39);
+            this.Buy.TabIndex = 6;
+            this.Buy.Text = "Buy";
+            this.Buy.UseVisualStyleBackColor = true;
+            this.Buy.Click += new System.EventHandler(this.Buy_Click);
+            // 
+            // Sell
+            // 
+            this.Sell.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Sell.Location = new System.Drawing.Point(486, 56);
+            this.Sell.Name = "Sell";
+            this.Sell.Size = new System.Drawing.Size(109, 39);
+            this.Sell.TabIndex = 7;
+            this.Sell.Text = "Sell";
+            this.Sell.UseVisualStyleBackColor = true;
+            this.Sell.Click += new System.EventHandler(this.Sell_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(482, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Roubles";
+            // 
+            // Roubles
+            // 
+            this.Roubles.AutoSize = true;
+            this.Roubles.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Roubles.Location = new System.Drawing.Point(593, 9);
+            this.Roubles.Name = "Roubles";
+            this.Roubles.Size = new System.Drawing.Size(18, 20);
+            this.Roubles.TabIndex = 9;
+            this.Roubles.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(314, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Currency";
+            // 
+            // Currency
+            // 
+            this.Currency.AutoSize = true;
+            this.Currency.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Currency.Location = new System.Drawing.Point(424, 9);
+            this.Currency.Name = "Currency";
+            this.Currency.Size = new System.Drawing.Size(18, 20);
+            this.Currency.TabIndex = 11;
+            this.Currency.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 514);
+            this.Controls.Add(this.Currency);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Roubles);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Sell);
+            this.Controls.Add(this.Buy);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputPrice);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputPrice)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown inputPrice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Buy;
+        private System.Windows.Forms.Button Sell;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Roubles;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Currency;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+using System.Collections.Specialized;
+using System.Collections;
+
+namespace SimulationLab16
+{
+    public partial class Form1 : Form
+    {
+        private const double dT = 0.005f;
+        private const double Mu = 0.1d;
+        private const double Xi = 0.02d;
+        private double W = 0;
+        private double price;
+        private Random random = new Random(Guid.NewGuid().GetHashCode());
+        private double cost;
+        private double roubles = 200;
+        private int currency = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 1000;
+            Roubles.Text = roubles.ToString();
+        }
+
+        private double GetNumber()
+        {
+            return Math.Sqrt(-2 * Math.Log(random.NextDouble())) * Math.Cos(2 * Math.PI * random.NextDouble());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            cost = (float)inputPrice.Value;
+            chart1.Series[0].Points.AddXY(0, cost);
+            price = cost;
+            currency = 0;
+            Currency.Text = currency.ToString();
+            roubles = 200;
+            Roubles.Text = roubles.ToString();
+            timer1.Start();
+        }
+
+        private void Buy_Click(object sender, EventArgs e)
+        {
+            if (roubles >= cost)
+            {
+                roubles -= cost;
+                Roubles.Text = roubles.ToString();
+                currency++;
+                Currency.Text = currency.ToString();
+            }
+        }
+
+        private void Sell_Click(object sender, EventArgs e)
+        {
+            if (currency > 0)
+            {
+                roubles += cost;
+                currency--;
+                Roubles.Text = roubles.ToString();
+                Currency.Text = currency.ToString();
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            chart1.ChartAreas[0].AxisY.Minimum = Math.Round(cost - 2, 4);
+            W += Math.Sqrt(dT) * GetNumber();
+            price *= Math.Exp((Mu - Xi * Xi / 2) * dT + Xi * W);
+            chart1.Series[0].Points.AddXY(0, price);
+            cost = (float)price;
+        }
+    }
+}
